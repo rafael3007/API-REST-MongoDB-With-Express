6cacf8633c98137516703d39513195c0a0386f0f
rafael3007 <rafael.brito.14222@gmail.com>
2022-04-03 09:45:19 -0300

    more

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ const app = express();
 app.use((req,res,next)=>{
     console.log('Controle de acesso realizado!')
     res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
     res.header("Access-Control-Allow-Headers: Content-Type")
     next()
