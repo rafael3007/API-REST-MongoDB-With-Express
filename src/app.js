133589901ae5aae59b486f87ba1a2dde6f21859a
rafael3007 <rafael.brito.14222@gmail.com>
2022-04-03 09:41:04 -0300

    cors-headers

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ app.use((req,res,next)=>{
     console.log('Controle de acesso realizado!')
     res.header("Access-Control-Allow-Origin","*");
     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    res.header("Access-Control-Allow-Headers: Content-Type")
     next()
 })
 
